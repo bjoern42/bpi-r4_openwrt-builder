b8ea18f63f7bdc1c9ae8310707ac07cd1364cfc7
bjoern <bjoern.eschle@disroot.org>
2026-03-29 17:06:25 +0200

    mend

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-rm -rf openwrt
-
-# checkout openwrt snapshot + cherry pick wifi hotfix
-git config user.email "bjoern.eschle@disroot.org"
-git config user.name "bjoern.eschle"
-
-
-git clone https://github.com/openwrt/openwrt.git openwrt
 cd openwrt;
-git remote add wifiHotfix https://github.com/danpawlik/openwrt.git
-git fetch wifiHotfix
-git cherry-pick 4235ed7d6d0d66da384bd487e3ed899c3b315efc
 
 #download external toolchain
 curl -s -L https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst | tar --zstd -xvf - -C .
---
+++ get-openwrt-sources.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf openwrt
+
+# checkout openwrt snapshot + cherry pick wifi hotfix
+git config user.email "bjoern.eschle@disroot.org"
+git config user.name "bjoern.eschle"
+
+
+git clone https://github.com/openwrt/openwrt.git openwrt
+cd openwrt;
+git remote add wifiHotfix https://github.com/danpawlik/openwrt.git
+git fetch wifiHotfix
+git cherry-pick 4235ed7d6d0d66da384bd487e3ed899c3b315efc
+cd ..
