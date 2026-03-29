ebc6e78cb4870d940fa34037c2f56d0d8a5b29f0
bjoern <bjoern.eschle@disroot.org>
2026-03-29 17:03:08 +0200

    mend

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -17,13 +17,17 @@ git cherry-pick 4235ed7d6d0d66da384bd487e3ed899c3b315efc
 #download external toolchain
 curl -s -L https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst | tar --zstd -xvf - -C .
 
+# config
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-wget https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/config.buildinfo -O .config
+curl -o .config https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/config.buildinfo
+
 ./scripts/ext-toolchain.sh \
         --toolchain openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64/toolchain-aarch64_cortex-a53_gcc-14.3.0_musl/ \
         --overwrite-config \
         --config mediatek_filogic_DEVICE_bananapi_bpi-r4-poe
+
+make defconfig
 
 #build
 make -j $(nproc) defconfig download clean world
